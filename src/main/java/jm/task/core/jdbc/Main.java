424c88d9456b02e4046994c3773d662b251b00e1
DmitryDK3 <dmitrydk3@gmail.com>
2021-04-28 22:13:09 +0300

    tests passed

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+        userDaoJDBC.createUsersTable();
+        userDaoJDBC.saveUser("john","sss",(byte) 12);
+        userDaoJDBC.saveUser("cindy","fsfs",(byte) 112);
+        userDaoJDBC.saveUser("pavel","dfffff",(byte) 23);
+        userDaoJDBC.saveUser("ivan","bbbb",(byte) 75);
+        List<User> users = userDaoJDBC.getAllUsers();
+        for (User u : users) {
+            System.out.println(u);
+        }
+        userDaoJDBC.cleanUsersTable();
+        userDaoJDBC.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
-public class Util {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util  {
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/new_schema",
+                    "root",
+                    "Poopk!n47mysql"
+            );
+            System.out.println("Подключились");
+            return connection;
+        } catch (SQLException e) {
+            System.out.println("Непрокатило");;
+        }
+        return null;
+    }
+
+
     // реализуйте настройку соеденения с БД
 }
