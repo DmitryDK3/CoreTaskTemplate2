e8bc79a3bf0d19cdc5c525ef3dbdd705197a7467
DmitryDK3 <dmitrydk3@gmail.com>
2021-05-01 15:31:33 +0300

    tests passed

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,6 +3,7 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.SQLException;
@@ -10,17 +11,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-        userDaoJDBC.createUsersTable();
-        userDaoJDBC.saveUser("john","sss",(byte) 12);
-        userDaoJDBC.saveUser("cindy","fsfs",(byte) 112);
-        userDaoJDBC.saveUser("pavel","dfffff",(byte) 23);
-        userDaoJDBC.saveUser("ivan","bbbb",(byte) 75);
-        List<User> users = userDaoJDBC.getAllUsers();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("john","sss",(byte) 12);
+        userService.saveUser("cindy","fsfs",(byte) 112);
+        userService.saveUser("pavel","dfffff",(byte) 23);
+        userService.saveUser("ivan","bbbb",(byte) 75);
+        List<User> users = userService.getAllUsers();
         for (User u : users) {
             System.out.println(u);
         }
-        userDaoJDBC.cleanUsersTable();
-        userDaoJDBC.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
