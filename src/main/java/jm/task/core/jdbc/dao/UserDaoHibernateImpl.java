09ac80b7a55ee5d21baacc0d0c1971dfd87a2ae5
DmitryDK3 <dmitrydk3@gmail.com>
2021-05-04 16:41:55 +0300

    tests passed2

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -17,49 +17,36 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try {
             session = Util.setUp().openSession();
             Transaction tr = session.beginTransaction();
             session.createSQLQuery("CREATE TABLE IF NOT EXISTS myusers(id INT PRIMARY KEY AUTO_INCREMENT , name varchar(255), lastName varchar(255), age INT  )").executeUpdate();
             System.out.println("как будто создали таблицу");
             tr.commit();
             session.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void dropUsersTable() {
-        try {
             session = Util.setUp().openSession();
             Transaction tr = session.beginTransaction();
             session.createSQLQuery("DROP TABLE IF EXISTS myusers").executeUpdate();
             System.out.println("как будто удалили таблицу");
             tr.commit();
             session.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try {
             session = Util.setUp().openSession();
             Transaction tr = session.beginTransaction();
             session.save(new User(name,lastName,age));
             System.out.printf("User с именем – %s добавлен в базу данных", name);
             tr.commit();
             session.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void removeUserById(long id) {
-        try {
             session = Util.setUp().openSession();
             Transaction tr = session.beginTransaction();
             User user = (User) session.get(User.class,id);
@@ -67,15 +54,11 @@ public class UserDaoHibernateImpl implements UserDao {
             System.out.printf("Юзер %d удален",id);
             tr.commit();
             session.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public List<User> getAllUsers() {
         List<User> list = new ArrayList<>();
-        try {
             session = Util.setUp().openSession();
             Transaction tr = session.beginTransaction();
             list =  (List<User>) session.createQuery("from User").list();
@@ -83,23 +66,16 @@ public class UserDaoHibernateImpl implements UserDao {
             System.out.println("как будто получили лист юзеров");
             tr.commit();
             session.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         return list;
     }
 
     @Override
     public void cleanUsersTable() {
-        try {
             session = Util.setUp().openSession();
             Transaction tr = session.beginTransaction();
             session.createQuery("delete from User").executeUpdate();
             System.out.println("как будто очистили таблицу");
             tr.commit();
             session.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -10,7 +10,7 @@ public class User implements Serializable {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue (strategy = GenerationType.AUTO)
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
